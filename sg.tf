342b39a330a324b2ecd72ad657c498bcb4b045f9
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-07 23:36:04 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "main" {
+    name        = "SG-1"
+  description = "SG1"
+  vpc_id      = var.vpc
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "security_group_id" {
+  value = aws_security_group.main.id
+}
+
