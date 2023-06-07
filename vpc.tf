f0495c131e61e2f60136c394c21644b3e3fd9860
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-07 23:33:30 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,52 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.subnet
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route.id
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.subnet.id
+}
+
